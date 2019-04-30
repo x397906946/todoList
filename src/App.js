26e9a876d23d9496c3046349fd682d538c345cd1
Mr.X <397906946@qq.com>
2019-04-30 16:15:46 +0800

    基本框架与组件搭好

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './todoInput'
+import TodoItem from './todoItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        {id: 1, title: '第一个待办'},
+        {id: 2, title: '第二个待办'}
+      ]
+    }
+  }
+  render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return (
+        <li>
+          <TodoItem todo={item}/>
+        </li>
+      )
+    })
+    return (
+      <div className="App">
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}/>
+        </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Welcome from './welcome'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
+  <Welcome/>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
