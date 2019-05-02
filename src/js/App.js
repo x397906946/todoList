bc25b947985de05a76998b09ff13b0a0c5e44eba
Mr.X <397906946@qq.com>
2019-05-02 14:25:15 +0800

    消除 [不要直接修改state] 与 [需要onChange] 警告

--- src/js/App.js
+++ src/js/App.js
@@ -74,8 +74,9 @@ class App extends Component {
     this.setState(this.state)
   }
   onSignUp(user){
-    this.state.user = user
-    this.setState(this.state)
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
+    this.setState(stateCopy)
   }
 }
 
--- src/js/UserDialog.js
+++ src/js/UserDialog.js
@@ -78,8 +78,15 @@ export default class UserDialog extends Component {
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
           <nav onChange={this.switch}>
-            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册</label>
-            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录</label>
+            <label>
+              <input type="radio" value="signUp"
+                     checked={this.state.selected === 'signUp'}
+                     onChange={this.switch}/> 注册
+            </label>
+            <label>
+              <input type="radio" value="signIn"
+                     checked={this.state.selected === 'signIn'}
+                     onChange={this.switch}/> 登录</label>
           </nav>
           <div className="panes">
             {this.state.selected === 'signUp' ? signUpForm : null}
