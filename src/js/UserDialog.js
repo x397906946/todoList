fbd4ecf769bee125685c7beba7d0c34f3ea0b887
Mr.X <397906946@qq.com>
2019-05-02 13:22:29 +0800

    添加 注册/登录 切换功能

--- src/js/UserDialog.js
+++ src/js/UserDialog.js
@@ -2,41 +2,59 @@ import React, {Component} from 'react';
 import '../css/UserDialog.css'
 
 export default class UserDialog extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 'signUp'
+    }
+    this.switch = this.switch.bind(this)
+  }
+  switch(e){
+    this.setState({
+      selected: e.target.value
+    })
+  }
   render() {
+    let signUpForm = (
+      <form className="signUp"> {/* 注册 */}
+        <div className="row">
+          <label>用户名</label>
+          <input type="text"/>
+        </div>
+        <div className="row">
+          <label>密码</label>
+          <input type="password"/>
+        </div>
+        <div className="row actions">
+          <button type="submit">注册</button>
+        </div>
+      </form>
+    )
+    let signInForm = (
+      <form className="signIn"> {/* 登录 */}
+        <div className="row">
+          <label>用户名</label>
+          <input type="text"/>
+        </div>
+        <div className="row">
+          <label>密码</label>
+          <input type="password"/>
+        </div>
+        <div className="row actions">
+          <button type="submit">登录</button>
+        </div>
+      </form>
+    )
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
-          <nav>
-            <input type="radio"/> 注册
-            <input type="radio"/> 登录
+          <nav onChange={this.switch}>
+            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册</label>
+            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录</label>
           </nav>
-          <div className="panes"> {/* 注册 */}
-            <form className="signUp">
-              <div className="row">
-                <label>用户名</label>
-                <input type="text"/>
-              </div>
-              <div className="row">
-                <label>密码</label>
-                <input type="password"/>
-              </div>
-              <div className="row actions">
-                <button type="submit">注册</button>
-              </div>
-            </form>
-            <form className="signIn"> {/* 登录 */}
-              <div className="row">
-                <label>用户名</label>
-                <input type="text"/>
-              </div>
-              <div className="row">
-                <label>密码</label>
-                <input type="password"/>
-              </div>
-              <div className="row actions">
-                <button type="submit">登录</button>
-              </div>
-            </form>
+          <div className="panes">
+            {this.state.selected === 'signUp' ? signUpForm : null}
+            {this.state.selected === 'signIn' ? signInForm : null}
           </div>
         </div>
       </div>
