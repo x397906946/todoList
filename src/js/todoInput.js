7a28f9c3d8b251d5cd652c4d918e229aaf7da01a
Mr.X <397906946@qq.com>
2019-05-03 15:35:48 +0800

    将表单变为纯函数

--- src/js/todoInput.js
+++ src/js/todoInput.js
@@ -1,22 +1,18 @@
 import React, {Component} from 'react'
 import '../css/todoInput.css'
 
-export default class TodoInput extends Component {
-  constructor(props){
-    super(props)
-    this.submit = this.submit.bind(this)
-    this.changeTitle = this.changeTitle.bind(this)
+function submit(props, e) {
+  if(e.key === 'Enter'){
+    props.onSubmit(e)
   }
-  render() {
-    return <input type="text" className="TodoInput" value={this.props.content}
-                  onKeyPress={this.submit} onChange={this.changeTitle}/>
-  }
-  submit(e){
-    if(e.key === 'Enter'){
-      this.props.onSubmit(e)
-    }
-  }
-  changeTitle(e){
-    this.props.onChange(e)
-  }
+}
+
+function changeTitle(props, e) {
+  props.onChange(e)
+}
+
+export default function (props) {
+  return <input type="text" className="TodoInput" value={props.content}
+                onKeyPress={submit.bind(null, props)}
+                onChange={changeTitle.bind(null, props)}/>
 }
